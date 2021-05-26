9d29413ffc2f9f4e346c9925e0c4e589094c4ee3
Kratttos <42486596+Kratttos@users.noreply.github.com>
2021-05-26 12:23:08 -0300

    inicio

---
+++ src/main/java/com/cefet/pc2/exercicios/placeholder.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cefet.pc2.exercicios;
+
+/**
+ *
+ * @author Everton
+ */
+public class placeholder {
+    public static void main(String[] args) {
+        //placeholder
+        System.out.println("placeholder");
+    }
+}
