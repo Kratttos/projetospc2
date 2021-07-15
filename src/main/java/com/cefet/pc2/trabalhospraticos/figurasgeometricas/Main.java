00317115edc872bea3073269a26e5284e8583f6c
Everton Ferreira <42486596+Kratttos@users.noreply.github.com>
2021-07-15 07:37:27 -0300

    Inicio do trabalho figuras Geometricas

---
+++ src/main/java/com/cefet/pc2/trabalhospraticos/figurasgeometricas/Main.java
@@ -0,0 +1,9 @@
+package com.cefet.pc2.trabalhospraticos.figurasgeometricas;
+
+import com.cefet.pc2.trabalhospraticos.figurasgeometricas.control.MainControl;
+
+public class Main {
+    public static void main(String[] args)  {
+        new MainControl().start();
+    }
+}
---
+++ src/main/java/com/cefet/pc2/trabalhospraticos/figurasgeometricas/control/MainControl.java
@@ -0,0 +1,8 @@
+package com.cefet.pc2.trabalhospraticos.figurasgeometricas.control;
+
+public class MainControl {
+
+    public void start()  {
+        System.out.println("to aqui");
+    }
+}
