9286f8a06f7aa0c7e1897023795dbec0249e1a4c
Everton Ferreira <42486596+Kratttos@users.noreply.github.com>
2021-08-17 03:30:06 -0300

    inserir Reta

--- src/main/java/com/cefet/pc2/trabalhospraticos/figurasgeometricas/src/model/Reta.java
+++ src/main/java/com/cefet/pc2/trabalhospraticos/figurasgeometricas/src/model/Reta.java
@@ -1,4 +1,10 @@
 package com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.model;
 
 public class Reta extends Renderizavel{
+
+    private int tamanho;
+
+    public Reta(int tamanho) {
+        this.tamanho = tamanho;
+    }
 }
--- src/main/java/com/cefet/pc2/trabalhospraticos/figurasgeometricas/src/view/cli/RetaView.java
+++ src/main/java/com/cefet/pc2/trabalhospraticos/figurasgeometricas/src/view/cli/RetaView.java
@@ -3,12 +3,17 @@ package com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.view.cli;
 import com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.model.Renderizavel;
 import com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.model.Reta;
 
-public class RetaView extends SubMenuView{
+public class RetaView extends SubMenuView {
 
     @Override
     public Renderizavel askObject() {
-        System.out.println("PLACEHOLDER RETA : finge q aqui tem um menu");
-        return new Reta();
+
+        this.printLine("----Menu de Criação de Reta----");
+        this.printLine("Qual e o tamanho da reta ?");
+        int tamanho = this.askIntegerBiggerThanZero();
+
+        this.printLine("-----------------------------------");
+        return new Reta(tamanho);
     }
 
     @Override
