723cc33df4bceccf05a4d9a61ae39ca8a5e0edaa
Everton Ferreira <42486596+Kratttos@users.noreply.github.com>
2021-08-23 03:58:00 -0300

    Desenhar Texto e Reta

--- src/main/java/com/cefet/pc2/trabalhospraticos/figurasgeometricas/src/view/gui/Paint.java
+++ src/main/java/com/cefet/pc2/trabalhospraticos/figurasgeometricas/src/view/gui/Paint.java
@@ -1,9 +1,6 @@
 package com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.view.gui;
 
-import com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.model.Circulo;
-import com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.model.FigurasGeometricas;
-import com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.model.Quadrilateros;
-import com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.model.Renderizavel;
+import com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.model.*;
 import com.cefet.pc2.trabalhospraticos.figurasgeometricas.src.view.cli.MainView;
 
 import javax.swing.*;
@@ -41,16 +38,59 @@ public class Paint {
 
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            Graphics2D graphics = (Graphics2D) g;
             final int TAMANHO_TELA = (this.getSize().width) - 30;
+            final int INCREMENTO_PADRAO = 30;
             int posicaoInicialLado = 30;
             int posicaoInicialTopo = 20;
+
             int maiorHeight = 0;
             for (Renderizavel fig : this.figs) {
-                if (fig instanceof FigurasGeometricas) {
-                    Quadrilateros figura = (Quadrilateros) fig;
+                if (fig instanceof Quadrilateros) {
+                    if (fig instanceof Trapezio) {
+                        Trapezio trapezio = (Trapezio) fig;
+                        System.out.println("ainda vou fazer");
+                    } else {
+                        Quadrilateros figura = (Quadrilateros) fig;
 
-                    g.drawPolygon(figura.toPolygon());
+                        int height = figura.getAltura();
+                        int width = figura.getBase();
+
+                        if (maiorHeight < height) {
+                            maiorHeight = height;
+                        }
+
+                        if (posicaoInicialLado + width >= TAMANHO_TELA) {
+                            posicaoInicialTopo += (maiorHeight + INCREMENTO_PADRAO);
+                            posicaoInicialLado = INCREMENTO_PADRAO;
+                        }
+                        g.drawRect(posicaoInicialLado, posicaoInicialTopo, width, height);
+                        posicaoInicialLado += width + INCREMENTO_PADRAO;
+                    }
+                }
+                if (fig instanceof Texto) {
+                    Texto texto = (Texto) fig;
+                    int tamanhoTexto = texto.getText().length();
+
+                    g.drawString(texto.getText(), posicaoInicialLado, (posicaoInicialTopo * 2));
+
+                    posicaoInicialLado += (tamanhoTexto * 7) + INCREMENTO_PADRAO;
+                }
+                if (fig instanceof Triangulo) {
+                    Triangulo triangulo = (Triangulo) fig;
+                }
+
+                if (fig instanceof Reta) {
+                    Reta reta = (Reta) fig;
+
+                    int width = reta.getTamanho();
+
+                    g.drawLine(posicaoInicialLado, posicaoInicialTopo, (posicaoInicialLado + reta.getTamanho()), posicaoInicialTopo);
+
+                    if (posicaoInicialLado + width >= TAMANHO_TELA) {
+                        posicaoInicialTopo += (maiorHeight - INCREMENTO_PADRAO);
+                        posicaoInicialLado = INCREMENTO_PADRAO;
+                    }
+
                 }
                 if (fig instanceof Circulo) {
                     Circulo circ = (Circulo) fig;
@@ -62,12 +102,12 @@ public class Paint {
                     }
 
                     if (posicaoInicialLado + height >= TAMANHO_TELA) {
-                        posicaoInicialTopo += (maiorHeight - 30);
-                        posicaoInicialLado = 30;
+                        posicaoInicialTopo += (maiorHeight - INCREMENTO_PADRAO);
+                        posicaoInicialLado = INCREMENTO_PADRAO;
                     }
 
                     g.drawOval(posicaoInicialLado, posicaoInicialTopo, circ.getRaio(), circ.getRaio());
-                    posicaoInicialLado += circ.getRaio() + 30;
+                    posicaoInicialLado += circ.getRaio() + INCREMENTO_PADRAO;
                 }
 
             }
